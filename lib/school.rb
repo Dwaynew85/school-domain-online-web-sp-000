d3e0cf2a3c2d887005d0f70e06e9c9d8dd21e8e1
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 05:18:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,15 @@
 class School
-  attr_accessor :name, :roster
+  attr_accessor :name, :roster, :grade
 
   def initialize(name)
     @name = name
     @roster = {}
   end
+
+  def add_student(name, grade)
+    @name = name 
+    @grade = grade 
+    @roaster = {name.to_s, [grade]}
+  end 
 
 end
