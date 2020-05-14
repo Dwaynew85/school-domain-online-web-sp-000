896f7b3bac9f6d40374a42226fb73e218961be84
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-14 05:27:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   def add_student(name, grade)
     @name = name
     @grade = grade
-    if @roster[grade].uniq
+    unless @roster[@grade].include(@grade)
       @roster[@grade] = []
     end
     @roster[@grade] << @name
